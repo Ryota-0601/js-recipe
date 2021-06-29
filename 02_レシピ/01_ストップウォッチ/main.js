8848584097c3a32d082ad626af0a6550742b6975
Ryota-0601 <ryota-rk6@docomo.ne.jp>
2021-06-30 00:07:52 +0900

    [ストップウォッチ]を学習しました

--- 02_レシピ/01_ストップウォッチ/main.js
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -1,1 +1,22 @@
+const display = document.getElementById("display")
+const startButton = document.getElementById("startButton")
 
+let count = 0
+
+const countUp = function() {
+  count += 1
+  display.textContent = count / 100
+}
+
+let id = null
+
+startButton.onclick = function() {
+  if (id === null) {
+    id = setInterval(countUp, 10)
+    startButton.textContent = "stop"
+  } else {
+    clearInterval(id)
+    id = null
+    startButton.textContent = "start"
+  }
+}
