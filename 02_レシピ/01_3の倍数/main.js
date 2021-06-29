1db20d9687970c080d68d89ac38cc517aa2984cc
Ryota-0601 <ryota-rk6@docomo.ne.jp>
2021-06-29 13:00:29 +0900

    3の倍数を確認しました

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,10 @@
+const genkiFunction = function(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      console.log(`${i}!!!!!!`)
+    } else {
+      console.log(i)
+    }
+  }
+}
+genkiFunction(100)
