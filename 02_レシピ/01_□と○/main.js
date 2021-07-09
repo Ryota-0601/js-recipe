022eca3cffc46592f32ca3b694cdcd251b03e336
Ryota-0601 <ryota-rk6@docomo.ne.jp>
2021-07-09 18:51:42 +0900

    [ワーク1をやりました]を学習しました

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,20 @@
 const figure = document.getElementById("figure")
 
+/*
 figure.onclick = function() {
   figure.classList.toggle("rounded")
+}
+*/
+
+figure.onclick = function() {
+  if (figure.classList.contains("square")) {
+    figure.classList.add("rounded")
+  } else if (figure.classList.contains("square rounded")) {
+    figure.classList.remove("square rounded")
+
+    figure.classList.add("triangle")
+  } else if (figure.classList.contains("triangle")) {
+    figure.classList.add("square")
+    figure.classList.remove("triangle")
+  }
 }
--- 02_レシピ/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -1,12 +1,43 @@
 const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
+const container2 = document.getElementById("cards-container2")
+const inputElement2 = document.getElementById("input-todo2")
+const container3 = document.getElementById("cards-container3")
+const inputElement3 = document.getElementById("input-todo3")
 
 addButton.onclick = function() {
   const card = createCard(inputElement.value)
   container.append(card)
 
   inputElement.value = ""
+}
+
+inputElement.onkeypress = function(e) {
+  if (e.keyCode === 13) {
+    const card = createCard(inputElement.value)
+    container.append(card)
+
+    inputElement.value = ""
+  }
+}
+
+inputElement2.onkeypress = function(e) {
+  if (e.keyCode === 13) {
+    const card = createCard(inputElement2.value)
+    container2.append(card)
+
+    inputElement2.value = ""
+  }
+}
+
+inputElement3.onkeypress = function(e) {
+  if (e.keyCode === 13) {
+    const card = createCard(inputElement3.value)
+    container3.append(card)
+
+    inputElement3.value = ""
+  }
 }
 
 const createCard = function(text) {
@@ -29,3 +60,39 @@ const createCard = function(text) {
 
   return card
 }
+
+/*
+inputElement.addEventListener("keypress", enter)
+
+const enter = function(e) {
+  if (e.keyCode === 13) {
+    const text = inputElement.value
+
+    //cardを作成
+    const card = document.createElement("div")
+    card.className = "card"
+
+    //todoを作成
+    const todo = document.createElement("div")
+    todo.className = "todo"
+    todo.textContent = text
+
+    card.append(todo)
+
+    //deleteButtonを作成
+    const deleteButton = document.createElement("div")
+    deleteButton.className = "delete"
+    deleteButton.onclick = function() {
+      card.remove()
+    }
+    card.append(deleteButton)
+
+    container.append(card)
+
+    inputElement.value = ""
+  } else {
+    return
+  }
+}
+
+*/
